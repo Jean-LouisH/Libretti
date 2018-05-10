f7dc7381910b05a797c60f97a99e93d2f7c2419e
Jean-LouisH <louis_ml@live.co.uk>
2018-05-10 00:56:25 -0400

    Provided a data structure to define all relevant sub structures with a single init function and free function for simplicity, implemented core functions.

--- Libretti/Libretti.c
+++ Libretti/Libretti.c
@@ -1,6 +1,8 @@
 #include "include/Libretti.h"
 
+#include <SDL.h>
 #include "include/Compiler.h"
+#include "include/Callback.h"
 #include "include/File.h"
 #include "include/Mixer.h"
 #include "include/WaveformGenerator.h"
@@ -13,25 +15,57 @@ int lb_libraryCompilationTest()
 	return a;
 }
 
+void lb_initLibretti(Libretti* libretti)
+{
+	lb_initCallbackData(&libretti->callbackData, &libretti->audio, &libretti->noteWaves);
+	lb_initAudioRuntime(&libretti->runtime, &libretti->callbackData);
+}
+
 void lb_initCallbackData(lb_CallbackData* callbackData, lb_Audio* audio, lb_NoteWaves* noteWaves)
 {
 	callbackData->audio = audio;
 	callbackData->noteWaves = noteWaves;
 }
 
-void lb_initAudioRuntime(lb_Runtime* runtime, lb_CallbackData* callbackData, void* callback)
+void lb_initAudioRuntime(lb_Runtime* runtime, lb_CallbackData* callbackData)
 {
+	if (SDL_Init(SDL_INIT_AUDIO) < 0)
+	{
+		SDL_Log("SDL_Init failed: %s", SDL_GetError());
+	}
+	else
+	{
+		SDL_AudioSpec desired;
+		SDL_AudioSpec obtained;
 
+		SDL_memset(&desired, 0, sizeof(desired));
+		desired.freq = SAMPLE_FREQUENCY;
+		desired.format = AUDIO_S16SYS;
+		desired.channels = CHANNELS;
+		desired.samples = SAMPLE_SIZE;
+		desired.callback = (SDL_AudioCallback)runCallback;
+		desired.userdata = callbackData;
+
+		SDL_AudioDeviceID device = SDL_OpenAudioDevice(
+			NULL,
+			0,
+			&desired,
+			&obtained,
+			NULL);
+
+		runtime->device = device;
+		lb_play(runtime);
+	}
 }
 
-void lb_compileAudioFromScript(lb_Audio* audio, char* filename)
+void lb_compileAudioFromScriptFile(lb_Audio* audio, char* filename)
 {
 	char* script = loadScriptFromFile(filename);
 	compileAudioFromScript(audio, script);
 	free(script);
 }
 
-void lb_updateNotesFromAudio(lb_Note* notes, lb_Audio* audio)
+void lb_updateNotesFromAudio(lb_Note* notes, unsigned char* noteCount, lb_Audio* audio)
 {
 
 }
@@ -41,7 +75,7 @@ void lb_updateNoteWavesFromAudio(lb_NoteWaves* noteWaves, lb_Audio* audio)
 
 }
 
-void lb_updateNoteWavesFromNotes(lb_NoteWaves* noteWaves, lb_Note* notes)
+void lb_updateNoteWavesFromNotes(lb_NoteWaves* noteWaves, lb_Note* notes, unsigned char* noteCount)
 {
 
 }
@@ -56,7 +90,8 @@ void lb_incrementPlayTime(lb_Runtime* runtime, double timeSeconds)
 
 void lb_load(lb_Runtime* runtime, lb_Audio* audio, char* filename)
 {
-
+	lb_compileAudioFromScriptFile(audio, filename);
+	lb_play(runtime);
 }
 
 void lb_play(lb_Runtime* runtime)
@@ -80,4 +115,9 @@ void lb_stop(lb_Runtime* runtime)
 {
 	lb_reset(runtime);
 	lb_pause(runtime);
+}
+
+void lb_freeLibretti(Libretti* libretti)
+{
+	SDL_CloseAudioDevice(libretti->runtime.device);
 }
--- Libretti/include/Libretti.h
+++ Libretti/include/Libretti.h
@@ -15,7 +15,7 @@
 #define Libretti_h
 
 #include "Audio.h"
-#include "Callback.h"
+#include "CallbackData.h"
 #include "Notes.h"
 #include "NoteWaves.h"
 #include "Runtime.h"
@@ -25,19 +25,29 @@ extern "C"
 {
 #endif
 
+	typedef struct
+	{
+		lb_NoteWaves noteWaves;
+		lb_Audio audio;
+		lb_CallbackData callbackData;
+		lb_Runtime runtime;
+	}Libretti;
+
 	int lb_libraryCompilationTest();
+	void lb_initLibretti(Libretti* libretti);
 	void lb_initCallbackData(lb_CallbackData* callbackData, lb_Audio* audio, lb_NoteWaves* noteWaves);
-	void lb_initAudioRuntime(lb_Runtime* runtime, lb_CallbackData* callbackData, void* callback);
-	void lb_compileAudioFromScript(lb_Audio* audio, char* filename);
-	void lb_updateNotesFromAudio(lb_Note* notes, lb_Audio* audio);
+	void lb_initAudioRuntime(lb_Runtime* runtime, lb_CallbackData* callbackData);
+	void lb_compileAudioFromScriptFile(lb_Audio* audio, char* filename);
+	void lb_updateNotesFromAudio(lb_Note* notes, unsigned char* noteCount, lb_Audio* audio);
 	void lb_updateNoteWavesFromAudio(lb_NoteWaves* noteWaves, lb_Audio* audio);
-	void lb_updateNoteWavesFromNotes(lb_NoteWaves* noteWaves, lb_Note* notes);
+	void lb_updateNoteWavesFromNotes(lb_NoteWaves* noteWaves, lb_Note* notes, unsigned char* noteCount);
 	void lb_incrementPlayTime(lb_Runtime* runtime, double timeSeconds);
 	void lb_load(lb_Runtime* runtime, lb_Audio* audio, char* filename);
 	void lb_play(lb_Runtime* runtime);
 	void lb_pause(lb_Runtime* runtime);
 	void lb_reset(lb_Runtime* runtime);
 	void lb_stop(lb_Runtime* runtime);
+	void lb_freeLibretti(Libretti* libretti);
 
 #ifdef __cplusplus
 }
