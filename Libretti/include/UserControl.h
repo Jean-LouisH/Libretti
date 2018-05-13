efc88375d1c602998f07c5d9847ece7f4d5342a1
Jean-LouisH <louis_ml@live.co.uk>
2018-05-12 20:31:24 -0400

    Added struct to contain application user control signals to libretti for effects and play back control.

---
+++ Libretti/include/UserControl.h
@@ -0,0 +1,21 @@
+/**
+*                      This file is part of:
+*                          Libretti
+*
+* User Control
+*
+* Represents the runtime controllable settings defined by the application.
+*
+* Copyright (c) 2017-2018 Jean-Louis Haywood. All rights reserved.
+* License: https://github.com/Jean-LouisH/Libretti/blob/master/LICENSE
+*/
+
+#ifndef UserControl_h
+#define UserControl_h
+
+typedef struct
+{
+
+}lb_UserControl;
+
+#endif /*UserControl_h*/
