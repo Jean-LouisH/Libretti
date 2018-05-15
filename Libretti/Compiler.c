12e1c3641cda77b4cd7e7e903245adcf7a3efb73
Jean-LouisH <louis_ml@live.co.uk>
2018-05-14 21:26:01 -0400

    Provided procedures for the compilaiton process.

--- Libretti/Compiler.c
+++ Libretti/Compiler.c
@@ -4,3 +4,38 @@ void compileAudioFromScript(lb_Audio* audio, char* script)
 {
 
 }
+
+void validateScript(char* script)
+{
+
+}
+
+void allocateMemory(lb_Audio* audio, char* script)
+{
+
+}
+
+void buildAudioData(lb_Audio* audio, char* script)
+{
+
+}
+
+void tuneByKeySignature(lb_Audio* audio, char* noteChar)
+{
+
+}
+
+void assignFrequencyFromNoteChar(lb_Note note, char noteChar)
+{
+
+}
+
+void tuneByAccidental(lb_Note note, char noteChar)
+{
+
+}
+
+void assignDurationValue(lb_Audio* audio, char* script)
+{
+
+}
--- Libretti/include/Compiler.h
+++ Libretti/include/Compiler.h
@@ -16,5 +16,12 @@
 #include "Audio.h"
 
 void compileAudioFromScript(lb_Audio* audio, char* script);
+void validateScript(char* script);
+void allocateMemory(lb_Audio* audio, char* script);
+void buildAudioData(lb_Audio* audio, char* script);
+void tuneByKeySignature(lb_Audio* audio, char* noteChar);
+void assignFrequencyFromNoteChar(lb_Note note, char noteChar);
+void tuneByAccidental(lb_Note note, char noteChar);
+void assignDurationValue(lb_Audio* audio, char* script);
 
 #endif /*Compiler_h*/
