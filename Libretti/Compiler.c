dd6a61ed22e3462955d78194f8bfb6ee4c31a928
Jean-LouisH <louis_ml@live.co.uk>
2018-08-30 19:26:18 -0400

    Began writing compilation code, frequency and duration assignments to be completed

--- Libretti/Compiler.c
+++ Libretti/Compiler.c
@@ -1,7 +1,9 @@
 #include "include/Compiler.h"
+#include "include/ScriptParseStates.h"
 #include "include/ScriptValidator.h"
 #include "include/Strings.h"
 #include <stdlib.h>
+#include <stdint.h>
 #include <stdbool.h>
 
 void compileAudioFromScript(lb_Audio* audio, char* script)
@@ -68,7 +70,6 @@ void allocateMemory(lb_Audio* audio, char* script)
 					noteCount++;
 			}
 		}
-
 		readPosition++;
 	} while (symbol != NULL);
 
@@ -82,7 +83,275 @@ void allocateMemory(lb_Audio* audio, char* script)
 
 void buildAudioData(lb_Audio* audio, char* script)
 {
+	int readPosition = 0;
+	int currentNote = 0;
+	int currentTrack = 0;
+	int currentTempoEvent = 0;
+	int currentLyricsEvent = 0;
 
+	uint8_t timeSigLower = 0;
+	uint8_t timeSigUpper = 0;
+	uint8_t octave = 0;
+	uint8_t tempo = 0;
+	bool tupletIsUnclosed = false;
+	bool slurIsUnclosed = false;
+	bool isReadingCrescendo = false;
+	bool isReadingDiminuendo = false;
+
+	uint8_t parseState = READING_NOTHING;
+	uint8_t previousParseState = 0;
+	lb_String header = newString("");
+	lb_String value = newString("");
+
+	do
+	{
+		switch (script[readPosition])
+		{
+		case '{':
+			parseState = READING_TRACK_SCOPE;
+			break;
+		case '[':
+			previousParseState = parseState;
+			parseState = READING_HEADER;
+			break;
+		case '_':
+			tupletIsUnclosed = !tupletIsUnclosed;
+			break;
+		case '~':
+			slurIsUnclosed = !slurIsUnclosed;
+			break;
+		case '<':
+			if (!isReadingCrescendo)
+				isReadingCrescendo = true;
+			else
+				isReadingCrescendo = false;
+			break;
+		case '>':
+			if (!isReadingDiminuendo)
+				isReadingDiminuendo = true;
+			else
+				isReadingDiminuendo = false;
+			break;
+		case ' ':
+			if (isReadingCrescendo)
+				isReadingCrescendo = false;
+
+			if (isReadingDiminuendo)
+				isReadingDiminuendo = false;
+
+			if (parseState == IGNORING_FIRST_SPACE_IN_VALUE)
+				parseState = READING_VALUE;
+			else if (parseState == READING_HEADER)
+				append(&header, script[readPosition]);
+			else if (parseState == READING_VALUE)
+				append(&value, script[readPosition]);
+			else if (parseState == READING_NOTE_DURATION)
+				parseState = READING_TRACK_SCOPE;
+			break;
+		case ':':
+			parseState = IGNORING_FIRST_SPACE_IN_VALUE;
+			break;
+		case ']':
+			if (strcmp(header.data, "time sig") == 0)
+			{
+				int valueReadPosition = 0;
+				lb_String upper = newString("");
+				lb_String lower = newString("");
+
+				while (value.data[valueReadPosition] != 0 &&
+					value.data[valueReadPosition] != '/')
+				{
+					append(&upper, value.data[valueReadPosition]);
+					valueReadPosition++;
+				}
+
+				valueReadPosition++;
+
+				while (value.data[valueReadPosition] != 0)
+				{
+					append(&lower, value.data[valueReadPosition]);
+					valueReadPosition++;
+				}
+
+				timeSigUpper = atoi(upper.data);
+				timeSigLower = atoi(lower.data);
+
+				//freeString(&upper);
+				//freeString(&lower);
+			}
+			else if (strcmp(header.data, "key sig") == 0)
+			{
+
+				if (strcmp(value.data, "C major") != 0 &&
+					strcmp(value.data, "G major") != 0 &&
+					strcmp(value.data, "D major") != 0 &&
+					strcmp(value.data, "A major") != 0 &&
+					strcmp(value.data, "E major") != 0 &&
+					strcmp(value.data, "B major") != 0 &&
+					strcmp(value.data, "Fs major") != 0 &&
+					strcmp(value.data, "Gb major") != 0 &&
+					strcmp(value.data, "Db major") != 0 &&
+					strcmp(value.data, "Ab major") != 0 &&
+					strcmp(value.data, "Eb major") != 0 &&
+					strcmp(value.data, "Bb major") != 0 &&
+					strcmp(value.data, "F major") != 0 &&
+					strcmp(value.data, "A minor") != 0 &&
+					strcmp(value.data, "E minor") != 0 &&
+					strcmp(value.data, "B minor") != 0 &&
+					strcmp(value.data, "Fs minor") != 0 &&
+					strcmp(value.data, "Cs minor") != 0 &&
+					strcmp(value.data, "Gs minor") != 0 &&
+					strcmp(value.data, "Ds minor") != 0 &&
+					strcmp(value.data, "Eb minor") != 0 &&
+					strcmp(value.data, "Bb minor") != 0 &&
+					strcmp(value.data, "F minor") != 0 &&
+					strcmp(value.data, "C minor") != 0 &&
+					strcmp(value.data, "G minor") != 0 &&
+					strcmp(value.data, "D minor") != 0)
+				{
+
+				}
+			}
+			else if (strcmp(header.data, "tempo") == 0)
+			{
+				tempo = atoi(value.data);
+				if (tempo == 0)
+				{
+					if (strcmp(value.data, "largo") != 0 &&
+						strcmp(value.data, "adagio") != 0 &&
+						strcmp(value.data, "adante") != 0 &&
+						strcmp(value.data, "moderato") != 0 &&
+						strcmp(value.data, "allegro") != 0 &&
+						strcmp(value.data, "presto") != 0)
+					{
+
+					}
+				}
+			}
+			else if (strcmp(header.data, "timbre") == 0)
+			{
+				if (strcmp(value.data, "square wave") != 0 &&
+					strcmp(value.data, "sine wave") != 0 &&
+					strcmp(value.data, "triangle wave") != 0 &&
+					strcmp(value.data, "sawtooth wave") != 0 &&
+					strcmp(value.data, "pulse 10") != 0 &&
+					strcmp(value.data, "pulse 25") != 0 &&
+					strcmp(value.data, "noise") != 0 &&
+					strcmp(value.data, "metallic") != 0)
+				{
+					lb_String filename = newString("Samples/");
+					lb_String extension = newString(".pcm");
+					strcat(filename.data, value.data);
+					strcat(filename.data, extension.data);
+					if (!exists(filename.data))
+					{
+
+					}
+					//freeString(&filename);
+					//freeString(&extension);
+				}
+			}
+			else if (strcmp(header.data, "loop") == 0)
+			{
+				int loops = atoi(value.data);
+				if (loops == 0)
+				{
+					if (strcmp(value.data, "none") != 0 &&
+						strcmp(value.data, "infinity") != 0)
+					{
+
+					}
+				}
+			}
+			else if (strcmp(header.data, "dynamic") == 0)
+			{
+				if (strcmp(value.data, "ppp") != 0 &&
+					strcmp(value.data, "pp") != 0 &&
+					strcmp(value.data, "p") != 0 &&
+					strcmp(value.data, "mp") != 0 &&
+					strcmp(value.data, "mf") != 0 &&
+					strcmp(value.data, "f") != 0 &&
+					strcmp(value.data, "ff") != 0 &&
+					strcmp(value.data, "fff") != 0)
+				{
+
+				}
+			}
+			else if (strcmp(header.data, "octave") == 0)
+			{
+				octave = atoi(value.data);
+			}
+			else if (strcmp(header.data, "panning") == 0)
+			{
+				float panning = atof(value.data);
+				if (panning == 0)
+				{
+					if (strcmp(value.data, "far left") != 0 &&
+						strcmp(value.data, "left") != 0 &&
+						strcmp(value.data, "mono") != 0 &&
+						strcmp(value.data, "right") != 0 &&
+						strcmp(value.data, "far right") != 0)
+					{
+
+					}
+				}
+				else if (panning < -1.0 && panning > 1.0)
+				{
+
+				}
+			}
+			parseState = previousParseState;
+			break;
+		case '}':
+			parseState = READING_NOTHING;
+			break;
+		case '+':
+			octave++;
+			break;
+		case '-':
+			octave--;
+			break;
+		default:
+			if (parseState == READING_HEADER)
+			{
+				append(&header, script[readPosition]);
+			}
+			else if (parseState == READING_VALUE)
+			{
+				append(&value, script[readPosition]);
+			}
+			else if (parseState == READING_TRACK_SCOPE)
+			{
+				if ((script[readPosition] >= 'A' && script[readPosition] <= 'G') ||
+					script[readPosition] == 'R')
+					parseState = READING_NOTE_FREQUENCY;
+			}
+			else if (parseState == READING_NOTE_FREQUENCY)
+			{
+				if (script[readPosition] == '#' || script[readPosition] == 'b' ||
+					script[readPosition] == 'n')
+				{
+					parseState = READING_NOTE_ACCIDENTAL;
+				}
+				else if (script[readPosition] >= '1' && script[readPosition] <= '9')
+				{
+					parseState = READING_NOTE_DURATION;
+					char substring[1];
+					substring[0] = script[readPosition];
+					double durationValue = atoi(substring);
+					if (tupletIsUnclosed)
+						durationValue = (durationValue * 2) / 3;
+
+				}
+			}
+			else if (parseState == READING_NOTE_ACCIDENTAL)
+			{
+				if (script[readPosition] >= '1' && script[readPosition] <= '9')
+					parseState = READING_NOTE_DURATION;
+			}
+		}
+		readPosition++;
+	} while (script[readPosition] != NULL);
 }
 
 void tuneByKeySignature(lb_Audio* audio, char* noteChar)
