6b9adf28618f121a6762d3e16c0680b7da2121bd
Jean-LouisH <louis_ml@live.co.uk>
2018-09-17 00:17:59 -0400

    Added compile switches for alternating between code for recording and playback

--- TestConsole/main.cpp
+++ TestConsole/main.cpp
@@ -4,6 +4,9 @@
 
 #include "ConsoleMenu.hpp"
 
+#define RECORD_TEST 0
+#define RECORDING_FILEPATH "audio_recording_dump.bin"
+
 #define MS_PER_S 1000.0
 
 int main(int argc, char* argv[])
@@ -17,20 +20,54 @@ int main(int argc, char* argv[])
 	bool isRunning = true;
 
 	menu(&fileName);
+#if !RECORD_TEST
 	Libretti* libretti = lb_createAndAddLibrettiToCallback(fileName.c_str());
+#else
+	lb_Binary_s16* binary = lb_captureAudio();
+
+	SDL_Window* window = SDL_CreateWindow(
+		"Libretti Test Console",
+		SDL_WINDOWPOS_CENTERED,
+		SDL_WINDOWPOS_CENTERED,
+		800,
+		600,
+		0);
+
+	lb_saveBinaryS16ToFile(binary, RECORDING_FILEPATH);
+#endif
 
 	do
 	{
 		cycleStart = SDL_GetTicks();
+#if !RECORD_TEST
 		lb_incrementAllPlayTimes(frameTime / MS_PER_S);
+#endif
 		cycleEnd = SDL_GetTicks();
 		double frameDelay = (MS_PER_S / fps) - (cycleEnd - cycleStart);
 		if (frameDelay > 0)
 			SDL_Delay(frameDelay);
 		frameTime = SDL_GetTicks() - cycleStart;
+
+#if RECORD_TEST
+		SDL_Event SDLEvents;
+
+		while (SDL_PollEvent(&SDLEvents))
+		{
+			switch (SDLEvents.type)
+			{
+			case SDL_KEYUP:
+				lb_saveBinaryS16ToFile(binary, RECORDING_FILEPATH);
+				break;
+			}
+		}
+#endif
 	} while (isRunning);
 
+#if !RECORD_TEST
 	lb_freeLibretti(libretti);
+#else
+	SDL_DestroyWindow(window);
+#endif
 	SDL_Quit();
 	return 0;
 }
